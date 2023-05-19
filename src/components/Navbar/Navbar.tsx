6b92f6e89e5a583575565409e54feb85322fed0a
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-05-19 18:57:15 -0300

    style: cleaning boilerplate styles

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Text, Navbar } from '@mantine/core'
+
+export default function MyNavbar() {
+    return (
+        <Navbar>
+            <Text component="h1">Hello World</Text>
+        </Navbar>
+    )
+}
