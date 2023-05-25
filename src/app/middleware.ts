dd9d91ae4e92f5b0cfaf190fca75623ec7e4b031
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-05-25 13:37:30 -0300

    chore(clerk): add /signUp to public routes

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-    publicRoutes: ['/', '/signIn'],
+    publicRoutes: ['/', '/signIn', '/signUp'],
 })
 
 export const config = {
