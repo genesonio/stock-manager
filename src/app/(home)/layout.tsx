486a5df81f359f8a222afab961986060f31611dd
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-05-25 13:38:29 -0300

    feat: add navbar to this layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,12 +7,8 @@ import { Navbar } from '@/components/Navbar'
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
-            <html lang="pt">
-                <body>
-                    <Navbar />
-                    {children}
-                </body>
-            </html>
+            <Navbar />
+            {children}
         </MantineProvider>
     )
 }
